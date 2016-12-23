1a053911726b4fce4807afc452b1974c885a9ed9
= <cm@an.tc>
2016-12-23 04:32:37 -0700

    Ditch unecessary type parameter.

--- core/src/main/scala/com.linktargeting.elasticsearch/api/syntax.scala
+++ core/src/main/scala/com.linktargeting.elasticsearch/api/syntax.scala
@@ -6,7 +6,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 trait syntax extends querySyntax {
 
-  class Syntax[Json, This <: Api, Response](f: ⇒ Future[Response])(implicit ec: ExecutionContext) {
+  class Syntax[Json, Response](f: ⇒ Future[Response])(implicit ec: ExecutionContext) {
     def apply(): Future[Response] = f
 
     def task: Future[Response] = f
@@ -21,36 +21,36 @@ trait syntax extends querySyntax {
   }
 
   implicit class IndexSyntax[Json](api: Index)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, Index, IndexResponse](client.document(api))
+    extends Syntax[Json, IndexResponse](client.document(api))
 
   implicit class BulkSyntax[Json](api: Bulk)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, Bulk, Seq[BulkResponse]](client.document(api))
+    extends Syntax[Json, Seq[BulkResponse]](client.document(api))
 
   implicit class DeleteIndexSyntax[Json](api: DeleteIndex)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, DeleteIndex, DeleteIndexResponse](client.indices(api))
+    extends Syntax[Json, DeleteIndexResponse](client.indices(api))
 
   implicit class RefreshSyntax[Json](api: Refresh)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, Refresh, RefreshResponse](client.indices(api))
+    extends Syntax[Json, RefreshResponse](client.indices(api))
 
   implicit class SearchSyntax[Json](api: Search)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, Search, SearchResponse[Json]](client.search(api)) with QueryOptionProvider[Search] {
+    extends Syntax[Json, SearchResponse[Json]](client.search(api)) with QueryOptionProvider[Search] {
     def withOptions(options: SearchOptions) = api.copy(
       query = QueryWithOptions(api.query, options)
     )
   }
 
   implicit class StartScrollSyntax[Json](api: StartScroll)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, StartScroll, ScrollResponse[Json]](client.search(api)) with QueryOptionProvider[StartScroll] {
+    extends Syntax[Json, ScrollResponse[Json]](client.search(api)) with QueryOptionProvider[StartScroll] {
     def withOptions(options: SearchOptions) = api.copy(
       query = QueryWithOptions(api.query, options)
     )
   }
 
   implicit class ScrollSyntax[Json](api: Scroll)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, Scroll, ScrollResponse[Json]](client.search(api))
+    extends Syntax[Json, ScrollResponse[Json]](client.search(api))
 
   implicit class ClearScrollSyntax[Json](api: ClearScroll)(implicit client: Dsl[Json], ec: ExecutionContext)
-    extends Syntax[Json, ClearScroll, Unit](client.search(api))
+    extends Syntax[Json, Unit](client.search(api))
 
 }
 
